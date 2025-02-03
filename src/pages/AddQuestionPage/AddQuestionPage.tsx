c28b36238735846e64ab3309b482f6bb0d25f6cf
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-02-03 00:15:36 -0500

    feat(add-question-page): add form

--- src/pages/AddQuestionPage/AddQuestionPage.tsx
+++ src/pages/AddQuestionPage/AddQuestionPage.tsx
@@ -1,8 +1,25 @@
-export default function AddQuestionPage() {
-    return (
-      <>
-        <h1>AddQuestionPage</h1>
-      </>
-    );
-  }
-  
+import AddQuestionForm from "../../components/AddQuestionForm/AddQuestionForm";
+import "./AddQuestionPage.css";
+
+/**
+ * `AddQuestionPage` Component
+ *
+ * This component renders a page for adding a new trivia question.
+ * It contains the `AddQuestionForm` component, which provides the actual form fields
+ * and submission logic.
+ *
+ * @component
+ * @example
+ * ```tsx
+ * <AddQuestionPage />
+ * ```
+ */
+const AddQuestionPage: React.FC = () => {
+  return (
+    <div className="add-question-page">
+      <AddQuestionForm />
+    </div>
+  );
+};
+
+export default AddQuestionPage;
